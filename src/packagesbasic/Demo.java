b9e8792fae2031cec4580fd49317ff53bd018192
Rashmitm984509 <160010929+Rashmitm984509@users.noreply.github.com>
2026-07-29 18:58:50 +0530

    Corejavaprogram

---
+++ src/packagesbasic/Demo.java
@@ -0,0 +1,29 @@
+package packagesbasic;// user defined pacakge
+
+import com.tnsif.accessmodifierprogram.Publicdemo;
+
+public class Demo {// class creation
+	
+	String h="roshn";
+	String H="roshn";
+	
+public static void main(String[]args) {// main method
+	
+	Publicdemo p=new Publicdemo();
+	
+	System.out.println(p.a);
+	//System.out.println(p.b);
+	//System.out.println(p.s);
+	
+	        Student s1 = new Student("Ravi");
+	        Student s2 = new Student("Priya");
+
+	        s1.display();
+	        s2.display();
+	    
+	
+	
+	
+	System.out.println("welcome to java");
+}
+}
